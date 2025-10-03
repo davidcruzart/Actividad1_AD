1628a5905707f0958d052fa30f9147cb63a45643
Xavi <xavier.valverde926@comunidadunir.net>
2025-10-03 12:29:25 +0200

    Add - Entrada.java -> Como archivo main Update - PedidosYa.java - > métodos nuevos para csv y dat

---
+++ src/Entrada.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class Entrada {
+    public static void main(String[] args) {
+        PedidosYa pedidos = new PedidosYa();
+        //pedidos.exportarPedidos("src/resources/pedidos.csv");
+        //pedidos.exportarClientes("src/resources/clientes.csv");
+        //pedidos.escribirObjetoPedidos("src/resources/pedidos.dat");
+       // pedidos.escribirObjetoClientes("src/resources/clientes.dat");
+       // pedidos.leerClienteObj("src/resources/clientes.dat");
+    }
+}
--- src/PedidosYa.java
+++ src/PedidosYa.java
@@ -1,16 +1,12 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PedidosYa {
 
-    public static void main(String[] args) {
 
-    }
-   public static void exportarClientes(String path){
+   public void exportarClientes(String path){
        List <Clientes>listaClientes = new ArrayList<>();
        listaClientes.add(new Clientes(1,"Xavi","xavi@email.com"));
        listaClientes.add(new Clientes(2,"Unai","unai@email.com"));
@@ -37,14 +33,104 @@ public class PedidosYa {
        }
 
    }
-    public static void exportarPedidos(String path){
+    public void exportarPedidos(String path){
         List <Pedidos>listaPedidos = new ArrayList<>();
         listaPedidos.add(new Pedidos(1, 1, "AgeOfEmpires II", 200));
         listaPedidos.add(new Pedidos(2, 2, "Battlefield 6", 100));
         listaPedidos.add(new Pedidos(3, 3, "Silk Song",  5));
         listaPedidos.add(new Pedidos(3, 3, "OSU", 1));
         listaPedidos.add(new Pedidos(3, 3, "Cookie Clicker", 50));
+
+        File file = new File(path);
+        PrintWriter printWriter = null;
+
+        try {
+            printWriter = new PrintWriter(new FileWriter(file, true));
+            printWriter.println("id, producto, cantidad");
+            for (Pedidos pedido : listaPedidos){
+                printWriter.println(pedido);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            printWriter.close();
+        }
+    }
+
+    public void escribirObjetoClientes(String path){
+       Scanner lector = new Scanner(System.in);
+       File file = new File(path);
+       FileOutputStream fos = null;
+       ObjectOutputStream oos = null;
+
+        try {
+            fos = new FileOutputStream(file);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(new Clientes(4, "Borja", "borja@gmail.com"));
+        } catch (FileNotFoundException e) {
+            System.out.println("El fichero no existe");
+        } catch (IOException e){
+            System.out.println("No hay permisos de escritura");
+        } finally {
+            try {
+                oos.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cerrado de fichero");
+            }
+        }
+    }
+
+    public void escribirObjetoPedidos(String path){
+        Scanner lector = new Scanner(System.in);
+        File file = new File(path);
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+
+
+        try {
+            fos = new FileOutputStream(file);
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject(new Pedidos(4, 4, "Balatro", 1));
+        } catch (FileNotFoundException e) {
+            System.out.println("El fichero no existe");
+        } catch (IOException e){
+            System.out.println("No hay permisos de escritura");
+        } finally {
+            try {
+                oos.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cerrado de fichero");
+            }
+        }
+    }
+    public void leerClienteObj(String path){
+       File file = new File(path);
+       FileInputStream fis = null;
+       ObjectInputStream ois = null;
+
+        try {
+            fis = new FileInputStream(file);
+            ois = new ObjectInputStream(fis);
+            Clientes cliente = (Clientes) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error, el fichero no existe");;
+        }catch (IOException e) {
+            System.out.println("No hay permisos de escritura");
+        } catch (ClassNotFoundException | ClassCastException e) {
+            System.out.println("Error clase de lectura");
+        } finally {
+            try {
+                ois.close();
+            } catch (IOException | NullPointerException e) {
+                System.out.println("Error en el cerrado del fichero");
+            }
+        }
     }
 
 }
 
+//*  public static void main(String[] args) {
+//        //Aquí va la lógica para la exportación y el path
+//
+//
+//    } */

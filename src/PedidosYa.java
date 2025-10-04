05b8cdd0872b1714ca0c23e13f7a9b4febd485ed
unaib <unaibabe@hotmail.com>
2025-10-04 18:28:14 +0200

    agregado mkdir en exportar PedidosYa

--- src/PedidosYa.java
+++ src/PedidosYa.java
@@ -6,54 +6,61 @@ import java.util.Scanner;
 public class PedidosYa {
 
 
-   public void exportarClientes(String path){
-       List <Clientes>listaClientes = new ArrayList<>();
-       listaClientes.add(new Clientes(1,"Xavi","xavi@email.com"));
-       listaClientes.add(new Clientes(2,"Unai","unai@email.com"));
-       listaClientes.add(new Clientes(3,"David", "david@email.com"));
+    public void exportarClientes(String path) {
+        List<Clientes> listaClientes = new ArrayList<>();
+        listaClientes.add(new Clientes(1, "Xavi", "xavi@email.com"));
+        listaClientes.add(new Clientes(2, "Unai", "unai@email.com"));
+        listaClientes.add(new Clientes(3, "David", "david@email.com"));
 
-       //Añado el File - Xavi.
+        //Añado el File - Xavi.
 
-       File file = new File(path);
-       PrintWriter printWriter = null;
+        File file = new File(path);
+        PrintWriter printWriter = null;
 
-       //añado el try catch y sus excepciones,
-       // OJO! TRUQUI:
-       // Seleccionas el codigo, te pones encima sale la opcion Surround alli metes try catch y te lo envuelve
-       File parentDir = file.getParentFile();
-       // Crear directorio si no existe
-       if (parentDir != null && !parentDir.exists()) {
-           parentDir.mkdirs();
-       }
-       try {
-           printWriter = new PrintWriter(new FileWriter(file, true)); //false se sobreescibe
-           printWriter.println("id, nombre, email");
-           for (Clientes cliente : listaClientes){
-               printWriter.println(cliente);
-           }
-       } catch (IOException e) {
-           System.out.println("Error entrada fichero");;
-       }finally{
-           if (printWriter != null){
-            printWriter.close(); }
-       }
+        //añado el try catch y sus excepciones,
+        // OJO! TRUQUI:
+        // Seleccionas el codigo, te pones encima sale la opcion Surround alli metes try catch y te lo envuelve
+        File parentDir = file.getParentFile();
+        // Crear directorio si no existe
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
+        try {
+            printWriter = new PrintWriter(new FileWriter(file, true)); //false se sobreescibe
+            printWriter.println("id, nombre, email");
+            for (Clientes cliente : listaClientes) {
+                printWriter.println(cliente);
+            }
+        } catch (IOException e) {
+            System.out.println("Error entrada fichero");
+            ;
+        } finally {
+            if (printWriter != null) {
+                printWriter.close();
+            }
+        }
 
-   }
-    public void exportarPedidos(String path){
-        List <Pedidos>listaPedidos = new ArrayList<>();
+    }
+
+    public void exportarPedidos(String path) {
+        List<Pedidos> listaPedidos = new ArrayList<>();
         listaPedidos.add(new Pedidos(1, 1, "AgeOfEmpires II", 200));
         listaPedidos.add(new Pedidos(2, 2, "Battlefield 6", 100));
-        listaPedidos.add(new Pedidos(3, 3, "Silk Song",  5));
+        listaPedidos.add(new Pedidos(3, 3, "Silk Song", 5));
         listaPedidos.add(new Pedidos(3, 3, "OSU", 1));
         listaPedidos.add(new Pedidos(3, 3, "Cookie Clicker", 50));
 
         File file = new File(path);
         PrintWriter printWriter = null;
-
+        File parentDir = file.getParentFile();
+        // Crear directorio si no existe
+        if (parentDir != null && !parentDir.exists()) {
+            parentDir.mkdirs();
+        }
         try {
             printWriter = new PrintWriter(new FileWriter(file, true));
             printWriter.println("id, producto, cantidad");
-            for (Pedidos pedido : listaPedidos){
+            for (Pedidos pedido : listaPedidos) {
                 printWriter.println(pedido);
             }
         } catch (IOException e) {
@@ -63,10 +70,10 @@ public class PedidosYa {
         }
     }
 
-    public void escribirObjetoClientes(String path){
-       File file = new File(path);
-       FileOutputStream fos = null;
-       ObjectOutputStream oos = null;
+    public void escribirObjetoClientes(String path) {
+        File file = new File(path);
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
 
         try {
             fos = new FileOutputStream(file);
@@ -75,7 +82,7 @@ public class PedidosYa {
             oos.writeObject(new Clientes(4, "Nayara", "nayara@gmail.com"));
         } catch (FileNotFoundException e) {
             System.out.println("El fichero no existe");
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("No hay permisos de escritura");
         } finally {
             try {
@@ -86,7 +93,7 @@ public class PedidosYa {
         }
     }
 
-    public void escribirObjetoPedidos(String path){
+    public void escribirObjetoPedidos(String path) {
         File file = new File(path);
         FileOutputStream fos = null;
         ObjectOutputStream oos = null;
@@ -98,7 +105,7 @@ public class PedidosYa {
             oos.writeObject(new Pedidos(3, 1, "AgeOfEmpires II", 3));
         } catch (FileNotFoundException e) {
             System.out.println("El fichero no existe");
-        } catch (IOException e){
+        } catch (IOException e) {
             System.out.println("No hay permisos de escritura");
         } finally {
             try {
@@ -108,29 +115,31 @@ public class PedidosYa {
             }
         }
     }
-    public void leerClienteObj(String path){
-       File file = new File(path);
-       FileInputStream fis = null;
-       ObjectInputStream ois = null;
+
+    public void leerClienteObj(String path) {
+        File file = new File(path);
+        FileInputStream fis = null;
+        ObjectInputStream ois = null;
 
         try {
             fis = new FileInputStream(file);
             ois = new ObjectInputStream(fis);
             Clientes cliente = null;
 
-            while ((cliente = (Clientes) ois.readObject()) != null){
+            while ((cliente = (Clientes) ois.readObject()) != null) {
                 System.out.println(cliente.getId());
                 System.out.println(cliente.getNombre());
                 System.out.println(cliente.getEmail());
             }
 
-        }catch (FileNotFoundException e) {
-            System.out.println("Error, el fichero no existe");;
-        }catch(EOFException e){
+        } catch (FileNotFoundException e) {
+            System.out.println("Error, el fichero no existe");
+            ;
+        } catch (EOFException e) {
             System.out.println("Fin de fichero");
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("No hay permisos de escritura");
-        }catch (ClassNotFoundException | ClassCastException e) {
+        } catch (ClassNotFoundException | ClassCastException e) {
             System.out.println("Error clase de lectura");
         } finally {
             try {
@@ -140,7 +149,8 @@ public class PedidosYa {
             }
         }
     }
-    public void leerPedidosObj(String path){
+
+    public void leerPedidosObj(String path) {
         File file = new File(path);
         FileInputStream fis = null;
         ObjectInputStream ois = null;
@@ -150,7 +160,7 @@ public class PedidosYa {
             ois = new ObjectInputStream(fis);
             Pedidos pedido = null;
 
-            while ((pedido = (Pedidos) ois.readObject()) != null){
+            while ((pedido = (Pedidos) ois.readObject()) != null) {
                 System.out.println(pedido.getId());
                 System.out.println(pedido.getClienteId());
                 System.out.println(pedido.getCantidad());
@@ -158,10 +168,11 @@ public class PedidosYa {
             }
 
         } catch (FileNotFoundException e) {
-            System.out.println("Error, el fichero no existe");;
-        }catch(EOFException e){
+            System.out.println("Error, el fichero no existe");
+            ;
+        } catch (EOFException e) {
             System.out.println("Fin de fichero");
-        }catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("No hay permisos de escritura");
         } catch (ClassNotFoundException | ClassCastException e) {
             System.out.println("Error clase de lectura");
@@ -176,8 +187,3 @@ public class PedidosYa {
 
 }
 
-//*  public static void main(String[] args) {
-//        //Aquí va la lógica para la exportación y el path
-//
-//
-//    } */
